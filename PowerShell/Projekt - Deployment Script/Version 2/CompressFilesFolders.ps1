16d6d26ca63ba6366d05f8f6bae0c47e25e7b93c
Haui <ahauenstein@it-logix.ch>
2019-10-14 16:35:46 +0200

    Schritt 2 : Write Host hinzugefügt, Schritt : Connection-file wird nun ersetzt, Schritt 4: Wieder zusammenpacken tut noch nicht ganz richtig.

--- PowerShell/Projekt - Deployment Script/Version 2/CompressFilesFolders.ps1
+++ PowerShell/Projekt - Deployment Script/Version 2/CompressFilesFolders.ps1
@@ -5,6 +5,7 @@ Get-ChildItem  $FolderPath -Name  -Recurse -include *.pbix   |  rename-item -new
 
 #
 # Schritt 2: ZIP entpacken
+$FolderPath      = 'C:\Temp\Upload\' 
 Get-ChildItem  $FolderPath -Directory  -Recurse |
 Expand-Archive -Path "C:\Temp\Upload\*.zip" -DestinationPath "C:\Temp\Upload\PBIX_Entzippt"
 
@@ -13,13 +14,27 @@ Get-ChildItem "C:\Temp\Upload\" -Name  -Recurse -include *.zip |
         Expand-Archive -Path "C:\Temp\Upload\" -DestinationPath "C:\Temp\Upload\`.name" 
     }
 
-    Get-ChildItem $FolderPath -Filter *.zip | Expand-Archive -DestinationPath 'C:\Temp\Upload\TESTzIP\.ZIP' -Force    
-#
-# Schritt 3: Connedction File austauschen
+
+# !!!! Code Funktioniert  !!!!! #                  !!! OK !!!
+$FolderPath      = 'C:\Temp\Upload\' 
+$i=1
+
+Write-Host "Folgendes Zip-File is vorhanden : "
+Get-ChildItem $FolderPath -Filter *.zip | 
+    ForEach-Object{ $i=$i ++; Write-Host "$i.) $_"
+    }
+
+Get-ChildItem $FolderPath -Filter *.zip | Expand-Archive -DestinationPath 'C:\Temp\Upload\TESTzIP\' -Force 
+
 
 #
-# # Schritt 4: Verzeichnis wieder zu zip packen
-# compress-archive -Path "C:\Users\ahau\Downloads\TEST_7_ZIP\AHAU" -DestinationPath "C:\Users\ahau\Downloads\TEST_7_ZIP\AHAU.zip"
+# Schritt 3: Connedction File austauschen           !!! OK !!!
+Copy-Item -path 'C:\Temp\Upload\DEV\Connections' -Destination 'C:\Temp\Upload\TESTzIP\' 
+
+
+#
+# Schritt 4: Verzeichnis wieder zu zip packen
+compress-archive - -Path "C:\Temp\Upload\TESTzIP" -DestinationPath "C:\Temp\Upload\TESTzIP"
 
 # #
 # # Schritt 5: ZIP wieder auf PBIX renamen
