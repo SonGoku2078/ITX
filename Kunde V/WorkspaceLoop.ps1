4f66b359e1da5c261673be4637ff1e499fc00d89
Haui <ahauenstein@it-logix.ch>
2020-01-29 16:50:42 +0100

    code aufgeräumt und mit kommentaren versehen

--- Kunde V/WorkspaceLoop.ps1
+++ Kunde V/WorkspaceLoop.ps1
@@ -6,20 +6,21 @@ Connect-PowerBIServiceAccount -Credential $credential
 
 $Workspace   = Get-PowerBIWorkspace | Select-Object Name, ID #| Format-Table
 
-clear-content C:\Workspace3.txt
+clear-content C:\Workspace4.txt 
 $i=0
-Write-Host "#   00"
+
+# loop over all Workspaces
 $Workspace | ForEach-Object  {
 
                 $WS_ID   = $_.ID
                 $WS_NAme = $_.Name   
                 $i = $i + 1
-                Write-Host $i
+                Write-Host "Loop : $($i) | $($_.Name) | $($_.ID)" 
                                       
                 $uri      = "https://api.powerbi.com/v1.0/myorg/groups/$($WS_ID)/users" 
                 $response = Invoke-PowerBIRestMethod -Url $uri -Method Get | ConvertFrom-Json
 
-                # Select the necessary objects
+                # Select necessary objects
                 $Output1  = $response.value | Select-Object emailAddress,groupUserAccessRight, displayName,identifier,principalType # | Format-Table | Out-String -Width 160 
 
                 # Add the Workspace Name & ID
@@ -27,15 +28,16 @@ $Workspace | ForEach-Object  {
                 $Output1 | Add-Member -Membertype NoteProperty -Name Name  -Value $WS_NAME
                 $Output1 | Add-Member -Membertype NoteProperty -Name RowNo -Value $i
                 
-                # Write the file in json formatAdd the Workspace Name & ID
-                $OutToFile += $Output1  #| Format-List # | ConvertTo-Json                
+                # move each set of users and workspaces to the output-variable
+                $OutToFile += $Output1  
                  
             } 
 
-            $OutToFile_JSON = $OutToFile | ConvertTo-Json 
+# convert to JSON format
+$OutToFile_JSON = $OutToFile | ConvertTo-Json 
 
-Write-Host " 99 -------"
+# write json file to console (on screen)
 Write-Output $OutToFile | ConvertTo-Json 
-Write-Host " 99 -------"
 
+# write json file to disk
 set-Content -Path "C:\Workspace4.txt" -Value $OutToFile_JSON # | ConvertTo-Json 
